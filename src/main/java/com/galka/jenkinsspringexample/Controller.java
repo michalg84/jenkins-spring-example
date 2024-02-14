8df948c6120b87a6562505504f8207af9df70d1b
Michał Gałka <michalgalka84@gmail.com>
2024-02-14 15:11:41 +0100

    fix Controller

--- src/main/java/com/galka/jenkinsspringexample/Controller.java
+++ src/main/java/com/galka/jenkinsspringexample/Controller.java
@@ -1,10 +1,11 @@
 package com.galka.jenkinsspringexample;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller()
-@RestController("webapp")
+@RestController
+@RequestMapping(path = "/")
 public class Controller {
     @GetMapping
     public String code() {
