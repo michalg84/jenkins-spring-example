19bce938c57c8adb873689b642f6ff2d91937774
Michał Gałka <michalgalka84@gmail.com>
2024-02-14 14:35:27 +0100

    change endpoint

--- src/main/java/com/galka/jenkinsspringexample/Controller.java
+++ src/main/java/com/galka/jenkinsspringexample/Controller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @org.springframework.stereotype.Controller()
-@RestController
+@RestController("webapp")
 public class Controller {
     @GetMapping
     public String code() {
